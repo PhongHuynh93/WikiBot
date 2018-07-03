aeec2b4a07a7fbbaf621556405ecbc1b88085285
Phong Huỳnh <p7157568@gmail.com>
2018-07-04 00:29:36 +0700

    add library

---
+++ app/src/main/java/example/test/phong/wikibot/MainActivity.kt
@@ -0,0 +1,19 @@
+package example.test.phong.wikibot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import example.test.phong.wikibot.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/example/test/phong/wikibot/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package example.test.phong.wikibot.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
